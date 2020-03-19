1c413fb8646f922bc2097a4156444bc7a32b74bc
2001228 <abimbolajohn123@gmail.com>
2020-03-19 22:07:35 +0000

    started lab7

---
+++ lab2/index.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en"
+      <head>
+          <meta charset="UTF-8">
+          <title>superhero system</title>
+      </head>
+<body>
+<header>
+    <h1>The Super- Superhero System</h1>
+    <h2>Superhero Home Page</h2>
+</header>
+<p>what would you like to do</p>
+<ul>
+    <li><a href="insertSuperhero.php">Insert a superhero</a></li>
+    <li><a href="displaySuperhero.php">Display all superhero</a></li>
+    <li><a href="battle.php">Insert a battle</a></li>
+    <li><a href="dispalyBattles.php">Display all battles</a></li>
+</ul>
+<?
+include ("db_connect.php");
+$sql_query =
+</body>
